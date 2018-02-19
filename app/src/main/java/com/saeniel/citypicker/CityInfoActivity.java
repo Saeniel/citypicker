0aea6d4e722f77e7595b46abd13210ad5edb064e
Saeniel <zheka.lesnov1996@gmail.com>
2018-02-19 03:04:11 +0300

    Разобрался с библиотекой

--- app/src/main/java/com/saeniel/citypicker/CityInfoActivity.java
+++ app/src/main/java/com/saeniel/citypicker/CityInfoActivity.java
@@ -1,12 +1,21 @@
 package com.saeniel.citypicker;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
 
 /**
  * Created by Saeniel on 18.02.2018.
@@ -14,16 +23,28 @@ import butterknife.ButterKnife;
 
 public class CityInfoActivity extends AppCompatActivity {
 
-    @BindView(R.id.imvCityPicture)
     ImageView imvCityPicture;
-
-    @BindView(R.id.tvCityDescription)
-    ImageView tvCityDescription;
+    TextView tvCityDescription;
+    LineChartView chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_info);
-        ButterKnife.bind(this);
+        chart = (LineChartView) findViewById(R.id.chart);
+
+        List<PointValue> values = new ArrayList<PointValue>();
+        values.add(new PointValue(0, 2));
+        values.add(new PointValue(1, 4));
+        values.add(new PointValue(2, 3));
+        values.add(new PointValue(3, 4));
+
+        Line line = new Line(values).setColor(Color.GREEN).setCubic(true);
+        List<Line> lines = new ArrayList<Line>();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+        chart.setLineChartData(data);
     }
 }
